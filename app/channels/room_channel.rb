5e8be8103df66ed464844ebc912ab2aeb9ba1aef
nathanbwright <nathanielbwright@gmail.com>
2017-10-15 16:13:54 -0600

    Add basic username functionality

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -8,6 +8,15 @@ class RoomChannel < ApplicationCable::Channel
   end
 
   def speak(data)
-    ActionCable.server.broadcast "room_channel", message: data["message"]
+    ActionCable.server.broadcast "room_channel", message: "#{@username}: #{data["message"]}"
+  end
+
+  def join_room(data)
+    @username = data.fetch("username") || "Anonymous"
+    ActionCable.server.broadcast "room_channel", message: "#{@username} has joined the chat."
+  end
+
+  def leave_room(data)
+    ActionCable.server.broadcast "room_channel", message: "#{@username} has left the chat."
   end
 end
